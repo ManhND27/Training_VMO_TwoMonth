302147887e72377f588074858f507efda8869378
ManhND <nguyenducmanh_t62@hus.edu.vn>
2021-07-13 10:47:32 +0700

    update

--- day4/KNN/KNN.py
+++ day4/KNN/KNN.py
@@ -1,11 +1,11 @@
 import numpy as np 
 from functions import *
 
-trainSet, testSet = loadData("./Iris.csv")
+trainSet, testSet = loadData("./Iris.txt")
 numOfRightAnwser = 0
 for item in testSet:
-    knn = kNearestNeighbor(trainSet, item, 5)
-    answer = findMostOccur(knn)
-    numOfRightAnwser += item[-1] == answer
-    print("label: {} -> predicted: {}".format(item[-1], answer))
+        knn = kNearestNeighbor(trainSet, item, 5)
+        answer = findMostOccur(knn)
+        numOfRightAnwser += item[-1] == answer
+        print("label: {} -> predicted: {}".format(item[-1], answer))
 print("Accuracy", numOfRightAnwser/len(testSet))
--- day4/KNN/functions.py
+++ day4/KNN/functions.py
@@ -3,22 +3,23 @@ from numpy.core.fromnumeric import transpose
 from numpy.lib import math
 from numpy.lib.function_base import delete
 import pandas as pd
-from numpy import genfromtxt
+from numpy import dtype, genfromtxt
 
 def loadData(path):
-    data = np.genfromtxt(path, delimiter=',')
+    data = np.loadtxt(path, delimiter = ',', dtype=str)
     data = np.array(list(data))
-    data = np.delete(data, 0, 0)
+    #data = np.delete(data, 0, 0)
     data = np.delete(data, 0, 1)
     np.random.shuffle(data)
     trainSet = data[:100]
     testSet = data[100:]
-    return trainSet, testSet
+    yield trainSet
+    yield testSet
 
 def calcDistancs(pointA, pointB, numOfFeature = 4):
     tmp = 0
     for i in range(numOfFeature):
-        tmp += (float(pointA[i]) + float(pointB)) ** 2
+        tmp += (float(pointA[i]) + float(pointB[i])) ** 2
     return math.sqrt(tmp)
 
 def kNearestNeighbor(trainSet, point, k):
@@ -42,3 +43,4 @@ def findMostOccur(arr):
             maxOccur = num
             ans = label
     return ans
+
